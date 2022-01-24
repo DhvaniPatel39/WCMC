7254ad74c346efc76ca79d08ab232a2e9ae135a6
DhvaniPatel39 <71722237+DhvaniPatel39@users.noreply.github.com>
2022-01-24 20:21:33 +0530

    Add files via upload

---
+++ prac_3/android/app/src/main/kotlin/com/example/prac_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prac_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ prac_3/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<MyApp> {
+  TextEditingController num1 = new TextEditingController();
+  TextEditingController num2 = new TextEditingController();
+  String result = '0';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PRACTICAL_3'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            // first input field
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  //labelText: 'Enter 1st number',
+                  hintText: 'Enter 1st number',
+                ),
+                keyboardType: TextInputType.number,
+                controller: num1,
+              ),
+            ),
+            //second input filed
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  //labelText: 'Enter 2nd number',
+                  hintText: 'Enter 2nd number',
+                ),
+                keyboardType: TextInputType.number,
+                controller: num2,
+              ),
+            ),
+            RaisedButton(
+              color: Colors.yellow,
+              child: Text('ADD'),
+              onPressed: () {
+                setState(() {
+                  int sum = int.parse(num1.text) + int.parse(num2.text);
+                  result = sum.toString();
+                });
+              },
+            ),
+            SizedBox(height: 15),
+            new Text(
+              result,
+              style: TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
